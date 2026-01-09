5ed9fad1b423de9d7f4c95db133e9c2a6ff1a81c
theophilusnn <theonenhanga@gmail.com>
2026-01-08 21:02:50 -0600

    fix: color closure warning
    - i get it now

--- src/lib/components/TitleBarDot.svelte
+++ src/lib/components/TitleBarDot.svelte
@@ -1,28 +1,40 @@
 <script lang="ts">
 	let { color, onclick }: { color: 'red' | 'green' | 'amber'; onclick: () => void } = $props();
-	let finalColor = $state('');
-	let ariaLabel = $state('');
-	if (color === 'red') {
-		ariaLabel = 'Close Window';
-		finalColor = '--color-red-500';
-	} else if (color === 'green') {
-		finalColor = '--color-green-500';
-		ariaLabel = 'Maximize / Restore Window';
-	} else if (color === 'amber') {
-		finalColor = '--color-amber-500';
-		ariaLabel = 'Minimize Window';
-	}
+
+	const colorData = $derived.by(() => {
+		if (color === 'red') {
+			return {
+				ariaLabel: 'Close Window',
+				finalColor: '--color-red-500'
+			};
+		} else if (color === 'green') {
+			return {
+				finalColor: '--color-green-500',
+				ariaLabel: 'Maximize / Restore Window'
+			};
+		} else if (color === 'amber') {
+			return {
+				finalColor: '--color-amber-500',
+				ariaLabel: 'Minimize Window'
+			};
+		} else {
+			return {
+				finalColor: '--color-gray-500',
+				ariaLabel: 'Unknown'
+			};
+		}
+	});
 </script>
 
-{#if finalColor != ''}
-	<button {onclick} class="cursor-pointer" aria-label={ariaLabel}>
+{#if colorData.finalColor != ''}
+	<button {onclick} class="cursor-pointer" aria-label={colorData.ariaLabel}>
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			width="18"
 			height="18"
 			viewBox="0 0 24 24"
-			stroke="var({finalColor})"
-			fill="var({finalColor})"
+			stroke="var({colorData.finalColor})"
+			fill="var({colorData.finalColor})"
 			stroke-width="0"
 			stroke-linecap="round"
 			stroke-linejoin="round"
