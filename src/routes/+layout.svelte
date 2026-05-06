db289221eab7f7e82172ee7bf483fe78ad4ee0b7
theophilusnn <theonenhanga@gmail.com>
2026-05-05 19:35:27 -0600

    fix: favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -57,7 +57,7 @@
 		name="description"
 		content="Theophilus Nenhanga, a software developer in Saskatchewan, Canada. I build web applications and systems software with C, TypeScript, Svelte, and more."
 	/>
-	<link rel="icon" href="/static/favicon.ico" />
+	<link rel="icon" href="/favicon.ico" />
 	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
 	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
